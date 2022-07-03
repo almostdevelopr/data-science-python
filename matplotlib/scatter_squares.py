c9cb6e1bfa0b7bcdb25c24b7fd3fc6b691d289e5
almostdevelopr <minhalam.dev@gmail.com>
2022-07-03 21:58:06 +0530

    Add savefig

--- matplotlib/scatter_squares.py
+++ matplotlib/scatter_squares.py
@@ -6,7 +6,7 @@ y_values = [x**2 for x in x_values]
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 
-ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Reds, s=10)
 
 # Title and labels
 ax.set_title("Square Numbers")
@@ -16,4 +16,5 @@ ax.set_ylabel("Square of Value")
 # axis value ranges for x and y axis
 ax.axis([0, 1100, 0, 1100000])
 
-plt.show()
+# bbox_incesh -> trims extra whitespaces around the pics
+plt.savefig("square_scatterd_colormap.png", bbox_inches='tight')
