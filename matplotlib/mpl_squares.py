4fa0765e513f04edbeccd1960a278d66fdfcfc76
almostdevelopr <minhalam.dev@gmail.com>
2022-07-03 21:04:28 +0530

    Add myplot squares

---
+++ matplotlib/mpl_squares.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+squares = [1, 4, 9, 16, 25]
+fig, ax = plt.subplots()
+ax.plot(squares)
+# ax represents a single plot in the figure
+
+# plt.show() -> opens matplotlib's viewer and display the plot
+plt.show()
